5766db9c98ed5b482e6e77add6b4effae4937a43
Omar Youssef <omaryoussef1621999@gmail.com>
2025-09-02 07:30:39 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import { clickLabelEvent, observeElements } from "./utils.js";
-
-const labelElements = document.querySelectorAll("label");
-const sectionElements = document.querySelectorAll("section");
-
-labelElements.forEach(clickLabelEvent);
-observeElements(sectionElements);
